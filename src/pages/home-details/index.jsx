f4b91e8e59a5bb8d93833ec2646808c7f3ae463d
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-08-05 18:06:06 +0200

    segundo commit

--- src/pages/home-details/index.jsx
+++ src/pages/home-details/index.jsx
@@ -16,10 +16,12 @@ fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`)
     console.log(details)
 
     return(
-        <div className='wrap-details'>
+        <div className='wrap-details p-3'>
             {details.drinks?.map((c) =>(
-                <div className="card-details" key={c.idDrink}>
-                    <h6>{c.strDrink}</h6>
+                <div className="card-details bg-warning" key={c.idDrink}>
+                    <img  className="w-100 border" src={c.strDrinkThumb} alt="foto de cocktails" />
+            
+            <p>{c.strDrink}</p>
                 </div>
             ))}
         </div>
